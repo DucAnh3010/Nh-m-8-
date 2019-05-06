f6b2bd321c127a1527d49899d11e0359897b0aca
DucAnh3010 <48240701+DucAnh3010@users.noreply.github.com>
2019-05-06 20:15:14 +0700

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-for(var j = 0; j<17;j++){  var y =  $('<img>',{src:'kanji/b1.'+j+'.png',});
-        y.css("width","100%");
-     
-       
-       
-        y.appendTo('#div2');}
-    });
