ac608082949e32ffb112c94dd98bafb8180478fb
nguyenbadung0209 <nguyenbadung02091998@gmail.com>
2019-05-06 16:26:57 +0700

    .

---
+++ test.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+for(var j = 0; j<17;j++){  var y =  $('<img>',{src:'kanji/b1.'+j+'.png',});
+        y.css("width","100%");
+     
+       
+       
+        y.appendTo('#div2');}
+    });
